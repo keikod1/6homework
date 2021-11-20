190a1f23d2e7d57cadbb5edf365435373e85b5dc
keikod1 <madragimova@icloud.com>
2021-11-20 11:15:26 +0600

    registr/login

--- 6 home work/main.swift
+++ 6 home work/main.swift
@@ -7,5 +7,71 @@
 
 import Foundation
 
-print("Hello, World!")
+print("registr/ log in")
+
+var account: [String: Int] = [
+    "keiko": 123456,
+    "dasi": 098765,
+    "nurs": 066986,
+    "emir": 567890,
+]
+
+var username = readLine()!
+
+
+var newusername = true
+var newpassword = true
+var open = true
+
+switch username{
+case "registr":
+    print("введите имя пользователя")
+    let registrusername = readLine()!
+for (key, value) in account{
+if registrusername == key {
+    newusername == true
+    print("user name занят!")
+    print("попробуйте другой")
+    var registr = readLine()
+    
+}else{
+    newusername == false
+}
+}
+print("успешно введите пароль:")
+    let password: String = readLine()!
+    if (password.count > 5 && !password.isEmpty){
+        print("успешно зарегистрировались!")
+    }else{
+        print("введите более 6 символов")
+    }
+case "log in":
+    print("введите имя пользователя")
+    let openusername = readLine()
+    for (key, value) in account{
+    if openusername == key {
+        open == true
+        print("user name найден введите пароль:")
+    }else{
+        open == false
+    }
+    }
+    let password = readLine()
+    for (key,value) in account{
+    if (Int (password!) ?? 0) == value{
+        newpassword == true
+        print("вы вошли в аккаунт:")
+        break
+    }else{
+        newpassword == false
+        print("неверный пароль")
+            }
+        }
+default:
+    print("error")
+}
+
+
+
+
 

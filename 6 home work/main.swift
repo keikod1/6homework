bd8c7d7ded0bea023132549ed7700085f9041a56
keikod1 <madragimova@icloud.com>
2021-11-21 17:12:36 +0600

    registration

--- 6 home work/main.swift
+++ 6 home work/main.swift
@@ -17,59 +17,89 @@ var account: [String: Int] = [
 ]
 
 var username = readLine()!
-
-
 var newusername = true
 var newpassword = true
 var open = true
+var newUserNameField = ""
+var test2 = true
+
+func registr(){
+    print("введите имя пользователя")
+let registrusername = readLine()!
+    
+for (key, _) in account {
+    if registrusername == key{
+    newusername == false
+    print("user name занят!")
+    print("попробуйте другой")
+    var newUsernameReadLine = readLine()!
+    newUserNameField = newUsernameReadLine
+    return registr()
+    test2 = false
+    break
+}else{
+    test2 == true
+}
+}
+
+    var test = true
+    while test {
+    print("Введите пароль")
+    var password: String = readLine()!
+        
+    if (password.count > 5 && !password.isEmpty && test2){
+        account.updateValue(Int(password) ?? 0, forKey: registrusername)
+        print("успешно зарегистрировались!")
+        print(account)
+        test = false
+        
+    }else if (password.count > 5 && !password.isEmpty && !test2){
+        account.updateValue(Int(password) ?? 0, forKey: newUserNameField)
+        
+        print("успешно зарегистрировались!")
+        print(account)
+        test = false
+    }else{
+            print("введите более 6 символов")
+        }
+
+    }
+}
 
 switch username{
 case "registr":
-    print("введите имя пользователя")
-    let registrusername = readLine()!
-for (key, value) in account{
-if registrusername == key {
-    newusername == true
-    print("user name занят!")
-    print("попробуйте другой")
-    var registr = readLine()
-    
-}else{
-    newusername == false
-}
-}
-print("успешно введите пароль:")
-    let password: String = readLine()!
-    if (password.count > 5 && !password.isEmpty){
-        print("успешно зарегистрировались!")
-    }else{
-        print("введите более 6 символов")
-    }
+    registr()
 case "log in":
     print("введите имя пользователя")
     let openusername = readLine()
-    for (key, value) in account{
+    for (key, _) in account{
     if openusername == key {
         open == true
         print("user name найден введите пароль:")
     }else{
         open == false
+        
     }
     }
     let password = readLine()
-    for (key,value) in account{
+    for (_,value) in account{
     if (Int (password!) ?? 0) == value{
         newpassword == true
         print("вы вошли в аккаунт:")
         break
     }else{
         newpassword == false
-        print("неверный пароль")
+        
             }
         }
 default:
     print("error")
 }
+
+
+
+print(account)
+
 
 
 
